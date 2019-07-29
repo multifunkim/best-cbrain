dedf028b07d0633ecc68b558adee38a4497cbe46
aliobaibk <43523316+aliobaibk@users.noreply.github.com>
2019-07-28 20:11:46 -0400

    Add functions for compilation

---
+++ for_build/best_cargparse.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# 
+# Help message for compiling BEst (meant to be used with the BEst compiling script)
+# 
+# Last revision: July, 2019
+# Maintainer: Obai Bin Ka'b Ali @aliobaibk
+# License: In the app folder or check GNU GPL-3.0.
+
+
+
+from argparse import ArgumentParser, RawTextHelpFormatter
+from datetime import datetime
+import os
+from sys import argv
+from sys import exit as sys_exit
+from textwrap import dedent
+
+
+
+def setup_abspath(iargs):
+    """Makes sure all paths are absolute.
+    """
+    
+    iargs['output_dir'] = os.path.abspath(iargs['output_dir'])
+    
+    return iargs
+
+
+
+def check_iargs_parser(iargs):
+    """Defines the possible arguments of the program, generates help and usage messages,
+    and issues errors in case of invalid arguments.
+    """
+    
+    parser = ArgumentParser(
+        description=dedent('''\
+        Brain Entropy is space and time (BEst)
+        ____________________________________________________________________________________
+         
+           8b    d8 88   88 88     888888 88     888888 88   88 88b 88 88  dP 88 8b    d8
+           88b  d88 88   88 88       88   88     88__   88   88 88Yb88 88odP  88 88b  d88
+           88YbdP88 Y8   8P 88  .o   88   88     88""   Y8   8P 88 Y88 88"Yb  88 88YbdP88
+           88 YY 88 `YbodP' 88ood8   88   88     88     `YbodP' 88  Y8 88  Yb 88 88 YY 88
+           ------------------------------------------------------------------------------
+                              Multimodal Functional Imaging Laboratory
+        ____________________________________________________________________________________
+         
+        '''),
+        formatter_class=RawTextHelpFormatter,
+        add_help=False,
+        allow_abbrev=False)
+
+    parser.add_argument('-h', '--help',
+                        action='help',
+                        help=dedent('''\
+                        Shows this help message and exits.
+                        ____________________________________________________________
+                        '''))
+    parser.add_argument('-d', '--output-dir', nargs=1, type=str,
+                        default='',
+                        help=dedent('''\
+                        The directory (absolute or relative) where to save the
+                        output file. If not specified, then the current directory
+                        will be used.
+                         
+                        (default: %(default)s)
+                        (type: %(type)s)
+                        ____________________________________________________________
+                        '''),
+                        metavar=('X'),
+                        dest='output_dir')
+    parser.add_argument('-n', '--file-name', nargs=1, type=str,
+                        default="best-" + datetime.now().strftime("%y%m%d%H%M%S"),
+                        help=dedent('''\
+                        The name of the generated archive.
+                         
+                        (default: %(default)s)
+                        (type: %(type)s)
+                        ____________________________________________________________
+                        '''),
+                        metavar=('X'),
+                        dest='file_name')
+    parser.add_argument('-c', '--matlab-cmd', nargs=1, type=str,
+                        default='matlab',
+                        help=dedent('''\
+                        The command how MATLAB is normally invoked.
+                         
+                        (default: %(default)s)
+                        (type: %(type)s)
+                        ____________________________________________________________
+                        '''),
+                        metavar=('X'),
+                        dest='matlab_cmd')
+    
+    oargs = vars(parser.parse_args(iargs))
+
+    # Hack: when (nargs=1) a list should not be returned
+    for k in ['output_dir', 'file_name', 'matlab_cmd']:
+        if type(oargs[k]) is list:
+            oargs[k] = oargs[k][0]
+
+    return oargs
+
+
+
+def check_iargs(iargs):
+    """Checks the integrity of the input arguments and returns the options if successful
+    """
+    
+    oargs = check_iargs_parser(iargs)
+    oargs = setup_abspath(oargs)
+    return oargs
+
+
+
+def main(iargs):
+    """Main function, checks the inputs to compile BEst and prints the info for the compiling routine
+    """
+
+    oargs = check_iargs(iargs)
+
+    print('"' + oargs['output_dir'] + '" "'  + oargs['file_name'] + '" "' + oargs['matlab_cmd'] + '"')
+
+    return sys_exit(0)
+
+
+
+############## Main
+if __name__ == "__main__":
+    main(argv[1:])
+    
---
+++ for_build/best_compile.bash
@@ -0,0 +1,155 @@
+#!/bin/bash
+#
+# Compile the BEst standalone application with MATLAB
+# 
+# Last revision: July, 2019
+# Maintainer: Obai Bin Ka'b Ali @aliobaibk
+# License: In the app folder or check GNU GPL-3.0.
+
+
+
+############## Function to check if help was requested
+function help_requested() {
+    [[ "$@" =~ (^|[[:space:]])"-h"($|[[:space:]]) ]] || \
+    [[ "$@" =~ (^|[[:space:]])"--help"($|[[:space:]]) ]]
+}
+
+
+
+############## Function to check a download status
+function check_download_status() {
+    exit_code="$1"
+    file_desc="$2"
+    if [[ "$exit_code" != 0 ]]; then
+        echo -e "\n\n\n     ***** Failed to download [""$file_desc""]"
+        echo -e "\n     - Please check (or report a bug at): https://github.com/multifunkim/best-cbrain"
+        echo -e "\n     - Closing the program\n"
+        exit 1
+    fi
+}
+
+
+
+# Inputs
+this_loc="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
+if [ ! -f "$this_loc"/LICENSE ]; then
+    echo -e "\n     - The license file is missing, downloading now..."
+
+    wget -q -P "$this_loc" "https://raw.githubusercontent.com/multifunkim/best-cbrain/master/LICENSE"
+    check_download_status $? "license file"
+fi
+
+if [ ! -f "$this_loc"/best_cargparse.py ]; then
+    echo -e "\n     - The file 'best_cargparse.py' is missing, downloading now..."
+
+    wget -q -P "$this_loc" "https://raw.githubusercontent.com/multifunkim/best-cbrain/master/for_build/best_cargparse.py"
+    check_download_status $? "best_cargparse.py"
+    chmod u+x "$this_loc"/best_cargparse.py
+fi
+
+oargs="$("$this_loc"/best_cargparse.py "$@")"
+if [[ $? != 0 ]]; then
+    echo -e "\n\n\n     ***** Something went wrong"
+    echo -e "\n     Closing the program\n"
+    exit 1
+  
+elif help_requested "$@"; then
+    echo "$oargs"
+    exit 0
+  
+fi
+
+output_dir="$(echo "$oargs" | cut -d '"' -f 2)"
+output_name="$(echo "$oargs" | cut -d '"' -f 4)"
+matlab_cmd="$(echo "$oargs" | cut -d '"' -f 6)"
+
+
+
+# Temporary directory
+tmp_dir="$(mkdir -p "$output_dir" && mktemp -d --tmpdir="$output_dir" tmp-XXXXX)"
+
+
+
+# URLs
+url_best_lib="https://api.github.com/repos/multifunkim/best-brainstorm/releases/latest"
+url_bst="https://raw.githubusercontent.com/brainstorm-tools/brainstorm3/master/external/brainentropy"
+url_main="https://raw.githubusercontent.com/multifunkim/best-cbrain/master/for_build/app_extra/BEst.m"
+
+
+
+############## Cleaning function
+clean () {
+    echo -e "\n\n\n     ***** Doing some cleaning, PLEASE WAIT"
+
+    rm -rf "$tmp_dir" >/dev/null 2>&1
+
+    echo -e "\n     - All cleaning done, BYE"
+}
+trap clean EXIT
+
+
+
+# BEst library directory and main function
+best_lib="$tmp_dir"/lib
+best_main="$tmp_dir"/BEst.m
+
+
+
+# Download best-brainstorm and unzip
+echo -e "\n     - Downloading the BEst library..."
+
+zip_url="$(curl -s "$url_best_lib" | grep zipball_url | cut -d '"' -f 4)"
+curl -s -L -o "$tmp_dir"/best-lib.zip "$zip_url"
+check_download_status $? "BEst library"
+unzip -q "$tmp_dir"/best-lib.zip -d "$best_lib"
+mv "$best_lib"/multifunkim-best-brainstorm*/* "$best_lib"
+rm -rf "$tmp_dir"/best-lib.zip "$best_lib"/multifunkim-best-brainstorm*
+
+
+
+# Download Brainstorm program files
+echo -e "\n     - Downloading the Brainstorm program files..."
+
+wget -q -P "$best_lib"/bst-files "$url_bst"/be_install.m
+check_download_status $? "be_install.m"
+wget -q -P "$best_lib"/bst-files "$url_bst"/be_main.m
+check_download_status $? "be_main.m"
+wget -q -P "$best_lib"/bst-files "$url_bst"/panel_brainentropy.m
+check_download_status $? "panel_brainentropy.m"
+
+
+
+# Download BEst main file
+echo -e "\n     - Downloading BEst main file..."
+
+wget -q -O "$best_main" "$url_main"
+check_download_status $? "BEst main file"
+
+
+
+# Compile
+echo -e "\n     - Compiling..."
+
+app_zip="$output_name".zip
+app_dir="$tmp_dir"/"${app_zip%'.zip'}"
+mkdir "$best_lib" "$app_dir"
+
+cp "$this_loc"/LICENSE "$app_dir"
+
+mcc_cmd='mcc -m '"$best_main"' -a '"$best_lib"' -d '"$app_dir"' -o BEst -R -nodisplay -v'
+"$matlab_cmd" -nodisplay -nosplash -r \
+'try, '"$mcc_cmd"', catch e, disp(getReport(e)), exit(1), end, exit(0);'
+
+if [[ $? != 0 ]]; then
+    echo -e "\n     - ERROR, compilation failed"
+    exit 1
+elif [ -z "$(ls -A "$app_dir")" ]; then
+    echo -e "\n     - ERROR, empty directory:\n""$app_dir""\n"
+    exit 1
+else
+    pushd "$app_dir" >/dev/null 2>&1
+    zip -qr "$output_dir"/"$app_zip" .
+    popd >/dev/null 2>&1
+    echo -e "\n     - All done, check:\n""$output_dir"/"$app_zip"
+fi
